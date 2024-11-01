c24b83b0a3f65b07e293edefca846ca9ade8fcbd
Mazen421 <mazenahmed421@yahoo.com>
2024-11-01 20:06:59 +0200

    First commit

---
+++ 21P0103_Mazen_Ahmed_Galal_Main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Maz on 11/1/2024.
+//
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int x,y;
+    std::cout << "enter first number: ";
+    std::cin >> x >> endl;
+    std::cout << "enter second number: ";
+    std::cin >> y >> endl;
+    std::cout << x+y;
+    return 0;
+}
